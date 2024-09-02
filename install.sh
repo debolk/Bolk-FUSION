d96837b3454867a7cdf2a7a901ee4cbeb1ac9ff2
Alchemi <alchemi1999@hotmail.com>
2024-09-02 17:06:57 +0200

    Added forwarding mail option

--- install.sh
+++ install.sh
@@ -6,6 +6,6 @@ scp contrib/openldap/* root@10.99.1.102:/etc/ldap/schema/fusiondirectory/
 scp -r locale/* root@10.99.1.102:/usr/share/fusiondirectory/locale/plugins
 scp -r personal/* root@10.99.1.102:/usr/share/fusiondirectory/plugins/personal
 
-insert="fusiondirectory-schema-manager --ldapuri ldap://10.99.1.101 --binddn cn=admin,cn=config --bindpwd WOWgeenGOSA2022! --simplebind --insert-schema"
+insert="fusiondirectory-schema-manager --ldapuri ldap://10.99.1.101 --binddn cn=admin,cn=config --bindpwd WOWgeenGOSA2022! --simplebind --replace-schema"
 
 ssh root@10.99.1.102 "fusiondirectory-configuration-manager --update-cache; ${insert} /etc/ldap/schema/fusiondirectory/bolkdata-fd.schema; ${insert} /etc/ldap/schema/fusiondirectory/bolkdata-avg-fd.schema; ${insert} /etc/ldap/schema/fusiondirectory/bolkdata-mail-fd.schema"
