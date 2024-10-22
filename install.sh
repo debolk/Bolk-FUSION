71efb92d88b78a1699651803267443d82ba2c4d9
Alchemi <alchemi1999@hotmail.com>
2024-10-22 23:07:19 +0200

    better install script

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-# install.sh <fusion ip> <ldap ip> <ldap passwd>
+cp -r admin/* /usr/share/fusiondirectory/plugins/admin
+cp -r contrib/openldap/* /etc/ldap/schema/fusiondirectory/
+cp -r locale/* /usr/share/fusiondirectory/locale/plugins
+cp -r personal/* /usr/share/fusiondirectory/plugins/personal
 
-scp -r admin/* root@$0:/usr/share/fusiondirectory/plugins/admin
-scp contrib/openldap/* root@$0:/etc/ldap/schema/fusiondirectory/
-scp -r locale/* root@$0:/usr/share/fusiondirectory/locale/plugins
-scp -r personal/* root@$0:/usr/share/fusiondirectory/plugins/personal
+insert="fusiondirectory-schema-manager --ldapuri ldap://$1 --binddn cn=admin,cn=config --bindpwd $2 --simplebind --insert-schema"
 
-insert="fusiondirectory-schema-manager --ldapuri ldap://$1 --binddn cn=admin,cn=config --bindpwd $2 --simplebind --replace-schema"
-
-ssh root@$0 "fusiondirectory-configuration-manager --update-cache; ${insert} /etc/ldap/schema/fusiondirectory/bolkdata-fd.schema; ${insert} /etc/ldap/schema/fusiondirectory/bolkdata-avg-fd.schema; ${insert} /etc/ldap/schema/fusiondirectory/bolkdata-mail-fd.schema"
+fusiondirectory-configuration-manager --update-cache
+${insert} /etc/ldap/schema/fusiondirectory/bolkdata-fd.schema
+${insert} /etc/ldap/schema/fusiondirectory/bolkdata-avg-fd.schema
+${insert} /etc/ldap/schema/fusiondirectory/bolkdata-mail-fd.schema
