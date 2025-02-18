3c029110cf15eba22eb58029ac52af3dbcbaeaac
Alchemi1963 <alchemi1999@hotmail.com>
2025-02-18 22:52:24 +0100

    Exit reinstall if no ldap uri is specified

--- reinstall.sh
+++ reinstall.sh
@@ -5,10 +5,16 @@ cp -r contrib/openldap/* /etc/ldap/schema/fusiondirectory/
 cp -r locale/* /usr/share/fusiondirectory/locale/plugins
 cp -r personal/* /usr/share/fusiondirectory/plugins/personal
 
-replace="fusiondirectory-schema-manager --ldapuri ldap://$1 --binddn cn=admin,cn=config --bindpwd $2 --simplebind --replace-schema"
 
 fusiondirectory-configuration-manager --update-cache
 fusiondirectory-configuration-manager --update-locales
+
+if [ -z "$1" ]; then
+    exit 0
+fi
+
+replace="fusiondirectory-schema-manager --ldapuri ldap://$1 --binddn cn=admin,cn=config --bindpwd $2 --simplebind --replace-schema"
+
 ${replace} /etc/ldap/schema/fusiondirectory/bolkdata-fd.schema
 ${replace} /etc/ldap/schema/fusiondirectory/bolkdata-avg-fd.schema
 ${replace} /etc/ldap/schema/fusiondirectory/bolkdata-mail-fd.schema
